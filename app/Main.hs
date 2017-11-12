20ffddac7189b5e7e5b6804febf18831a2412ba3
Nick Johnstone <ncwjohnstone@gmail.com>
2017-11-12 19:06:46 +1300

    Main now compiles file from argument

--- app/Main.hs
+++ app/Main.hs
@@ -2,12 +2,31 @@ module Main where
 
 import Lib
 import Text.Parsec
+import System.Environment
+import Data.Maybe
 
+
+head' :: [a] -> Maybe a
+head' list =
+  case length list of
+    0 -> Nothing
+    _ -> Just $ head list
 
 main :: IO ()
 main = do
-  let result = parseExpressionFromString "addOne n = 5 + n"
-  case result of
-    Right a ->
-      putStrLn $ show a ++ "\n" ++ printExpression a ++ "\n" ++ printWasm a
-    Left err -> putStrLn $ "Dun goofed" ++ show err
+  args <- getArgs
+
+  let maybeFilename = head' args
+
+  case maybeFilename of
+    Just filename -> do
+      contents <- readFile filename
+
+      let result = parseExpressionFromString contents
+
+      case result of
+        Right a ->
+          putStrLn $ show a ++ "\n" ++ printExpression a ++ "\n" ++ printWasm a
+        Left err -> putStrLn $ "Dun goofed" ++ show err
+    Nothing -> putStrLn "please provide a file to compile"
+
