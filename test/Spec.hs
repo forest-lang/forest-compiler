203ad9b5abcda73d100372f7565772dc1ee8f117
Nick Johnstone <ncwjohnstone@gmail.com>
2017-11-12 19:07:22 +1300

    Add genCall to test

--- test/Spec.hs
+++ test/Spec.hs
@@ -5,10 +5,11 @@ instance Arbitrary Expression where
   arbitrary = genExpression
 
 genExpression :: Gen Expression
-genExpression = oneof [genIdentifier, genNumber, genAssignment, genInfix]
+genExpression =
+  oneof [genIdentifier, genNumber, genAssignment, genInfix, genCall]
 
 genChar :: Gen Char
-genChar = elements (['a'..'z'] ++ ['A'..'Z'])
+genChar = elements (['a' .. 'z'] ++ ['A' .. 'Z'])
 
 genString :: Gen String
 genString = listOf1 genChar
@@ -16,13 +17,11 @@ genString = listOf1 genChar
 genIdentifier :: Gen Expression
 genIdentifier = do
   name <- genString
-
   return $ Identifier name
 
 genNumber :: Gen Expression
 genNumber = do
   number <- arbitrarySizedNatural
-
   return $ Number number
 
 genAssignment :: Gen Expression
@@ -30,30 +29,31 @@ genAssignment = do
   name <- genString
   args <- listOf genString
   expr <- genExpression
-
   return $ Assignment name args expr
 
 genOperator :: Gen Operator
-genOperator =
-  elements [Add, Subtract, Multiply, Divide]
+genOperator = elements [Add, Subtract, Multiply, Divide]
 
 genInfix :: Gen Expression
 genInfix = do
   operator <- genOperator
   a <- genNumber
   b <- genExpression
-
   return $ Infix operator a b
+
+genCall :: Gen Expression
+genCall = do
+  name <- genString
+  args <- listOf1 genIdentifier
+  return $ Call name args
 
 propParseAndPrint :: Expression -> Bool
 propParseAndPrint expr =
-  let
-    output = printExpression expr
-    reparsedExpr = parseExpressionFromString output
-  in
-    case reparsedExpr of
-      Right newExpr -> newExpr == expr
-      Left err -> False
+  let output = printExpression expr
+      reparsedExpr = parseExpressionFromString output
+  in case reparsedExpr of
+       Right newExpr -> newExpr == expr
+       Left err -> False
 
 main :: IO ()
 main = quickCheck propParseAndPrint
