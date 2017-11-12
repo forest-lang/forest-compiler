c08930b1885422a1cbfb4941df143481dd9212e9
Nick Johnstone <ncwjohnstone@gmail.com>
2017-11-12 17:08:08 +1300

    Compile simple program to WASM

--- app/Main.hs
+++ app/Main.hs
@@ -13,8 +13,10 @@ data Operator
   deriving (Show)
 
 data Expression
-  = Number Int
+  = Identifier String
+  | Number Int
   | Assignment String
+               [String]
                Expression
   | Infix Operator
           Expression
@@ -45,12 +47,23 @@ parseInfix = do
 parseDeclaration = do
   name <- many letter
   spaces
+  arguments <- many parseArgument
   char '='
   spaces
   value <- parseExpression
-  return $ Assignment name value
+  return $ Assignment name arguments value
+  where
+    parseArgument = do
+      name <- many1 letter
+      spaces
+      return name
 
-parseExpression = try parseInfix <|> parseDigit <|> parseDeclaration
+parseExpression =
+  try parseInfix <|> parseDigit <|> try parseDeclaration <|> parseIdentifier
+
+parseIdentifier = do
+  name <- many1 letter
+  return $ Identifier name
 
 parseString = do
   expr <- parseExpression
@@ -70,13 +83,49 @@ printExpression expr =
   case expr of
     Number n -> show n
     Infix op expr expr2 ->
-      printExpression expr ++
-      " " ++ operatorToString op ++ " " ++ printExpression expr2
-    Assignment name expr -> name ++ " = " ++ printExpression expr
+      unwords [printExpression expr, operatorToString op, printExpression expr2]
+    Assignment name args expr ->
+      name ++ " " ++ unwords args ++ " = " ++ printExpression expr
+    Identifier name -> name
+
+indent :: String -> Int -> String
+indent str level =
+  unlines $ map (\line -> replicate level ' ' ++ line) (lines str)
+
+printWasm :: Expression -> String
+printWasm expr = "(module\n" ++ indent (printWasmExpr expr) 2 ++ ")"
+  where
+    printWasmExpr expr =
+      case expr of
+        Number n -> "(i32.const " ++ show n ++ ")"
+        Assignment name args expr ->
+          "(export \"" ++
+          name ++
+          "\" (func $" ++
+          name ++
+          "))\n(func $" ++
+          name ++
+          " " ++
+          paramsString args ++
+          " " ++ "(result i32)\n" ++ indent ("(return \n" ++ indent (printWasmExpr expr) 2 ++ ")") 2 ++ ")"
+        Infix op expr expr2 ->
+          "(" ++
+          opString op ++
+          "\n" ++ indent (printWasmExpr expr ++ "\n" ++ printWasmExpr expr2) 2 ++ ")\n"
+        Identifier name -> "(get_local $" ++ name ++ ")\n"
+    paramsString args = unwords (paramString <$> args)
+    paramString arg = "(param $" ++ arg ++ " i32)"
+    opString op =
+      case op of
+        Add -> "i32.add"
+        Subtract -> "i32.subtract"
+        Multiply -> "i32.multiply"
+        Divide -> "i32.divide"
 
 main :: IO ()
 main = do
-  let result = parse parseString "" "x = 5 + 3 + 2"
+  let result = parse parseString "" "addOne n = 5 + n"
   case result of
-    Right a -> putStrLn $ show a ++ "\n" ++ printExpression a
+    Right a ->
+      putStrLn $ show a ++ "\n" ++ printExpression a ++ "\n" ++ printWasm a
     Left err -> putStrLn $ "Dun goofed" ++ show err
