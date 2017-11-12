5dae3449f4ee72c718d82412b88ed61731e2a7f6
Nick Johnstone <ncwjohnstone@gmail.com>
2017-11-12 17:58:00 +1300

    Write property test for parsing and printing expressions

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,59 @@
+import Lib
+import Test.QuickCheck
+
+instance Arbitrary Expression where
+  arbitrary = genExpression
+
+genExpression :: Gen Expression
+genExpression = oneof [genIdentifier, genNumber, genAssignment, genInfix]
+
+genChar :: Gen Char
+genChar = elements (['a'..'z'] ++ ['A'..'Z'])
+
+genString :: Gen String
+genString = listOf1 genChar
+
+genIdentifier :: Gen Expression
+genIdentifier = do
+  name <- genString
+
+  return $ Identifier name
+
+genNumber :: Gen Expression
+genNumber = do
+  number <- arbitrarySizedNatural
+
+  return $ Number number
+
+genAssignment :: Gen Expression
+genAssignment = do
+  name <- genString
+  args <- listOf genString
+  expr <- genExpression
+
+  return $ Assignment name args expr
+
+genOperator :: Gen Operator
+genOperator =
+  elements [Add, Subtract, Multiply, Divide]
+
+genInfix :: Gen Expression
+genInfix = do
+  operator <- genOperator
+  a <- genNumber
+  b <- genExpression
+
+  return $ Infix operator a b
+
+propParseAndPrint :: Expression -> Bool
+propParseAndPrint expr =
+  let
+    output = printExpression expr
+    reparsedExpr = parseExpressionFromString output
+  in
+    case reparsedExpr of
+      Right newExpr -> newExpr == expr
+      Left err -> False
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = quickCheck propParseAndPrint
