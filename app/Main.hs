bf9512be5717606af2783b868e2824151deb26b7
Nick Johnstone <ncwjohnstone@gmail.com>
2018-01-02 21:15:42 +1300

    Reformat

--- app/Main.hs
+++ app/Main.hs
@@ -1,10 +1,9 @@
 module Main where
 
+import Data.Maybe
 import Lib
 import System.Environment
-import Data.Maybe
 import Text.Megaparsec.Error
-
 
 head' :: [a] -> Maybe a
 head' list =
@@ -15,18 +14,15 @@ head' list =
 main :: IO ()
 main = do
   args <- getArgs
-
   let maybeFilename = head' args
-
   case maybeFilename of
     Just filename -> do
       contents <- readFile filename
-
       let result = parseExpressionFromString contents
-
       case result of
-        Right a ->
-          putStrLn $ printWasm a
-        Left err -> putStrLn $ "Syntax error in " ++ filename ++ "\n" ++ parseErrorPretty' contents err
+        Right a -> putStrLn $ printWasm a
+        Left err ->
+          putStrLn $
+          "Syntax error in " ++
+          filename ++ "\n" ++ parseErrorPretty' contents err
     Nothing -> putStrLn "please provide a file to compile"
-
