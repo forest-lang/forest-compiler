85c566ef96ee63c1d54b5733965afcc5d844c2d8
Nick Johnstone <ncwjohnstone@gmail.com>
2018-04-02 19:38:04 +1200

    Extract AST types to Language module

---
+++ src/Language.hs
@@ -0,0 +1,74 @@
+{-# LANGUAGE DeriveGeneric #-}
+
+module Language
+  ( NonEmptyString(..)
+  , OperatorExpr(..)
+  , Ident(..)
+  , Expression(..)
+  , Declaration(..)
+  , Annotation(..)
+  , Module(..)
+  , s
+  , idToString
+  , neToString
+  ) where
+
+import qualified Data.List.NonEmpty as NE
+import qualified Generics.Deriving as G
+
+newtype Module =
+  Module [Declaration]
+  deriving (Show, Eq, G.Generic)
+
+data Declaration =
+  Declaration (Maybe Annotation)
+              Ident
+              [Ident]
+              Expression
+  deriving (Show, Eq, G.Generic)
+
+data Annotation =
+  Annotation Ident
+             (NE.NonEmpty Ident)
+  deriving (Show, Eq, G.Generic)
+
+data Expression
+  = Identifier Ident
+  | Number Int
+  | Infix OperatorExpr
+          Expression
+          Expression
+  | Call Ident
+         [Expression]
+  | Case Expression
+         (NE.NonEmpty (Expression, Expression))
+  | Let (NE.NonEmpty Declaration)
+        Expression
+  | BetweenParens Expression
+  | String' String
+  deriving (Show, Eq, G.Generic)
+
+data OperatorExpr
+  = Add
+  | Subtract
+  | Divide
+  | Multiply
+  | StringAdd
+  deriving (Show, Eq, G.Generic)
+
+newtype Ident =
+  Ident NonEmptyString
+  deriving (Show, Eq)
+
+newtype NonEmptyString =
+  NonEmptyString (NE.NonEmpty Char)
+  deriving (Show, Eq)
+
+s :: Ident -> String
+s = idToString
+
+idToString :: Ident -> String
+idToString (Ident str) = neToString str
+
+neToString :: NonEmptyString -> String
+neToString (NonEmptyString se) = NE.toList se
